773e573469e7980c99f6bc8c35d45ebf954e71a1
孙昊翔 <673686754@qq.com>
2019-02-18 16:18:30 +0800

    no message

---
+++ 12-enum.js
@@ -0,0 +1,20 @@
+var Days;
+(function (Days) {
+    Days[Days["Sun"] = 0] = "Sun";
+    Days[Days["Mon"] = 1] = "Mon";
+    Days[Days["Tue"] = 2] = "Tue";
+    Days[Days["Wed"] = 3] = "Wed";
+    Days[Days["Thu"] = 4] = "Thu";
+    Days[Days["Fri"] = 5] = "Fri";
+    Days[Days["Sat"] = 6] = "Sat";
+})(Days || (Days = {}));
+var Days2;
+(function (Days2) {
+    Days2[Days2["Sun"] = 7] = "Sun";
+    Days2[Days2["Mon"] = 1] = "Mon";
+    Days2[Days2["Tue"] = 2] = "Tue";
+    Days2[Days2["Wed"] = 3] = "Wed";
+    Days2[Days2["Thu"] = 4] = "Thu";
+    Days2[Days2["Fri"] = 5] = "Fri";
+    Days2[Days2["Sat"] = 6] = "Sat";
+})(Days2 || (Days2 = {}));
---
+++ 12-enum.ts
@@ -0,0 +1,3 @@
+enum Days {Sun, Mon, Tue, Wed, Thu, Fri, Sat}
+
+enum Days2 {Sun = 7, Mon = 1, Tue, Wed, Thu, Fri, Sat}
