b050ab4f7ffb5fd3064859f5bbbe3a2759f3980e
孙昊翔 <673686754@qq.com>
2019-02-18 14:47:38 +0800

    no message

---
+++ 1-primitive-data-types.ts
@@ -0,0 +1,27 @@
+let isDone: boolean = false
+
+let createdByNewBoolean: Boolean = new Boolean(1)
+
+let createdByBoolean: boolean = Boolean(1)
+
+let decLiteral: number = 6
+let hexLiteral: number = 0xf00d
+// ES6 中的二进制表示法
+let binaryLiteral: number = 0b1010
+// ES6 中的八进制表示法
+let octalLiteral: number = 0o744
+let notANumber: number = NaN
+let infinityNumber: number = Infinity
+
+let myName: string = 'Tom'
+let myAge: number = 25
+
+function alertName(): void {
+  alert('My name is Tom')
+}
+
+let unusable: void = undefined
+
+let u: undefined = undefined
+
+let n: null = null
---
+++ 2-any.js
@@ -0,0 +1,6 @@
+var myFavoriteNumber = 'seven';
+myFavoriteNumber = 7;
+var anyThing = 'hello';
+console.log(anyThing.myName);
+console.log(anyThing.myName.firstName);
+anyThing.setName('Jetty');
