5707656cb8d1ade53941201101c7b539c3d07144
孙昊翔 <673686754@qq.com>
2023-04-08 19:22:36 +0800

    array tuple

--- basic-types.ts
+++ basic-types.ts
@@ -10,3 +10,15 @@ let u: undefined = undefined
 let n: null = null
 
 let num: number = undefined
+
+let notSure: any = 4
+notSure = 'maybe a string instead'
+notSure = true
+
+let numberOrString: number | string = 234
+numberOrString = 'abc'
+
+let arrOfNumbers: number[] = [1, 2, 3, 4]
+arrOfNumbers.push(5)
+
+let user: [string, number] = ['Alex', 20]
