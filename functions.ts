e4c4e84a4a7e43dae5fd9bafcc780b7a68acea09
孙昊翔 <673686754@qq.com>
2023-04-08 19:56:08 +0800

    functions

---
+++ functions.ts
@@ -0,0 +1,19 @@
+function add(x: number, y: number, z: number = 10): number {
+  if (typeof z === 'number') {
+    return x + y + z
+  } else {
+    return x + y
+  }
+}
+
+let result = add(2, 3)
+
+const add2 = (x: number, y: number, z: number = 10): number {
+  if (typeof z === 'number') {
+    return x + y + z
+  } else {
+    return x + y
+  }
+}
+
+const add3: (x: number, y: number, z?: number) => number = add2
