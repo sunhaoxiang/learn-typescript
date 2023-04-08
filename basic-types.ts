473202572e79ba72184e79ad84d6b144236dd3f0
孙昊翔 <673686754@qq.com>
2023-04-08 19:12:54 +0800

    feat: basic types

---
+++ basic-types.ts
@@ -0,0 +1,12 @@
+let isDone: boolean = false
+
+let age: number = 20
+let binaryNumber: number = 0b1111
+
+let firstName: string = 'Alex'
+let message: string = `Hello, ${firstName}, age is ${age}`
+
+let u: undefined = undefined
+let n: null = null
+
+let num: number = undefined
