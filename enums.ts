324c4d5d461c576ef77fed9b92bd4e8179f74ef8
孙昊翔 <673686754@qq.com>
2023-04-08 20:07:53 +0800

    enums

---
+++ enums.ts
@@ -0,0 +1,10 @@
+const enum Direction {
+  Up = 'UP',
+  Down = 'DOWN',
+  Left = 'LEFT',
+  Right = 'RIGHT'
+}
+const value: string = 'UP'
+if (value === Direction.Up) {
+  console.log('Go up!')
+}
